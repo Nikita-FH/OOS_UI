6bbc3e5492f820a9d2bb2bc12e964b2e32676c54
Nikita-FH <nd9824s@ad.fh-aachen.de>
2024-12-04 09:59:31 +0100

    Init eingeügt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
-package ui;
+import bank.PrivateBank;
+import bank.exceptions.PrivateBankAlreadyExistsException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -6,12 +7,30 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main extends Application {
+
+    public Main(){super();};
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    Path p = Paths.get(".\\src\\main\\resources\\PrivateBanken\\Sparkasse");
+    PrivateBank bank;
+
+    @Override
+    public void init(){
+        try {
+            if (p.toFile().exists()){
+                bank = new PrivateBank("Sparkasse",0.1,0.1,true);
+            }
+            else bank = new PrivateBank("Sparkasse",0.1,0.1,false);
+        } catch (PrivateBankAlreadyExistsException | IOException e) {throw new RuntimeException(e);}
+    }
+
 
     @Override
     public void start(Stage primaryStage) {
